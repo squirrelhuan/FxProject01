8c95f0f67212865f9979b366ffb6411e6b612bea
huan <978252909@qq.com>
2019-09-06 09:59:27 +0800

    20190906

--- src/view/CImageView.java
+++ src/view/CImageView.java
@@ -3,6 +3,8 @@ package view;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.net.URL;
+
 /**
  * Created by huan on 2017/11/25.
  */
@@ -10,10 +12,24 @@ public class CImageView extends ImageView {
 
     //"/fxml/image/folder_icon_16.png"
     public void setImageResource(String path){
-       String relpath = getClass().getClass()
-                .getResource(path)
-                .toString();
+        if(path==null){
+                System.out.println("image path is null");
+                return;
+        }
+        System.out.println(path);
+       URL url = getClass().getClass()
+                .getResource(path);
+       if(url==null){
+           System.out.println("url is null"+path);
+           setImageResourcePath("file:"+path);
+           return;
+       }
+        String relpath = url.toString();
         Image image = new Image(relpath);
        this.setImage(image);
+    }
+    public void setImageResourcePath(String path){
+        Image image = new Image(path);
+        this.setImage(image);
     }
 }
